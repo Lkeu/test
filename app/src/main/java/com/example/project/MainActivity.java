ea17c591dec701c8340bddc9f0f868fc669db760
Tim <timoxaduck@gmial.com>
2024-05-31 18:38:16 +0800

    in progress...

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -17,10 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
     }
 
     public void start(View v){
--- app/src/main/java/com/example/project/ServerActivity.java
+++ app/src/main/java/com/example/project/ServerActivity.java
@@ -1,21 +1,33 @@
 package com.example.project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class ServerActivity extends AppCompatActivity {
     private EditText messageText;
-    private TextView chatView;
+    private ListView messegeList;
     private DatabaseReference database;
+    DatabaseReference myRef;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +35,39 @@ public class ServerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_server);
 
         messageText = findViewById(R.id.messegeText);
-        chatView = findViewById(R.id.chatView);
+        messegeList= findViewById(R.id.messageList);
 
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://project-c880a-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        myRef = database.getReference("message");
+        myRef.setValue("");
+        ArrayList<String> messegeArr = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.simple_list_item1, messegeArr);
+        messegeList.setAdapter(adapter);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messegeArr.add(snapshot.getValue().toString());
+                Log.d("bla", snapshot.getValue().toString());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 
     public  void onClickSend(View view){
         String message = messageText.getText().toString();
-        database.push().setValue(message);
+        myRef.setValue(message);
+
 
     }
 
